d870ddc3f402fc597c65df4f9a5c6d64030aaf30
kaisagung <109098893+kaisagung@users.noreply.github.com>
2022-07-12 00:05:06 +0700

    Add files via upload

---
+++ soal 3.php
@@ -0,0 +1,36 @@
+<?php
+$bintang = 6;
+    for ( $a=$bintang;
+    $a>0; $a--) {
+        for ($i=1;
+        $i<=$a; $i++){
+            echo " &nbsp";
+        }
+        for ($a1=$bintang;$a1>=$a;
+        $a1--){
+            echo "*";
+        }
+        echo "<br>";
+    }
+
+    
+	$star=5;
+	for($a=$star;$a>0;$a--){
+	for($b=$star;$b>=$a;$b--){
+		echo "*";
+	}
+	echo "<br>";
+	}
+
+
+
+
+
+
+
+
+
+
+ ?>
+
+
---
+++ soal1.java
@@ -0,0 +1,27 @@
+/*!
+Isilah data pribadi : 
+
+Nama : Kais
+Alamat Email: kaisagungimaduddin@gmail.com
+Waktu Mulai :22.00
+Waktu Selesai :23.59
+
+*/
+
+
+(function_createclass() {
+var data1 = [1, 1, 1, 2, 2, 4, 1, 1 ]
+    data1.forEach(item =>   {
+    console.log(item);  // output:  1
+    });
+var data2 = [2, 1, 2, 2, 2, 8, 1, 1 ]
+    data2.forEach(item =>   {
+    console.log(item);  // output:  2
+    });
+var data3 = [3, 3, 3, 3, 2, 4, 1, 1 ]
+    data3.forEach(item =>   {
+    console.log(item); //output:    3
+    });
+
+    }
+)
